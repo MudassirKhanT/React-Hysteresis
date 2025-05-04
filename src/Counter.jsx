154b2e577a2c623330496f947229d82bd591d2e4
mudassir <mudassirkhantailor@gmail.com>
2025-05-04 22:17:50 +0530

    Added "Function:- Updator Function"

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
 function Counter() {
-  //Updator Function
+  //Updator Function => give an arrow function and perform the operation
+  //Updator function Name -> Precount
   const [count, setCount] = useState(0);
   function Increment() {
-    setCount(count + 1);
+    setCount((pre) => pre + 1);
+    setCount((pre) => pre + 1);
   }
 
   return (
